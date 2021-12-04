053cec55896d57b664fbc34bedc5bb57301aa1e2
Dustin Moris Gorski <dustinmoris@gmail.com>
2021-12-04 10:25:10 +0000

    hello world web server

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world")
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
+	addr := ":" + port
+
+	fmt.Printf("Starting web server, listening on %s\n", addr)
+
+	err := http.ListenAndServe(addr, http.HandlerFunc(webServer))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func webServer(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte("hello world"))
 }
